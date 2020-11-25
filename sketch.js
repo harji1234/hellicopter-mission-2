96102be2e508c16e8c7fc1caa9cb6c472eb31f73
harji1234 <73263036+harji1234@users.noreply.github.com>
2020-11-24 20:43:02 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var leftbody,rightbody,basebody
+var flag = 1;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	leftsprite=createSprite(width/2-100,610,20,100)
+	leftbody= Bodies.rectangle(width/2-100,610,20,100,{isStatic:true})
+	rightsprite=createSprite(width/2+100,610,20,100)
+	rightbody= Bodies.rectangle(width/2+100,610,20,100,{isStatic:true})
+	basesprite=createSprite(width/2,650,200,20)
+	basebody= Bodies.rectangle(width/2,650,200,20,{isStatic:true})
+	World.add(world, leftbody)
+	World.add(world, rightbody)
+	World.add(world, basebody)
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.0,isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+if(keyCode===LEFT_ARROW && flag===1){
+	helicopterSprite.x=helicopterSprite.x-20;  
+	translation={x:-20,y:0}
+	Matter.Body.translate(packageBody, translation)
+}
+if(keyCode===RIGHT_ARROW && flag ===1){
+	helicopterSprite.x=helicopterSprite.x+20;  
+	translation={x:20,y:0}
+		Matter.Body.translate(packageBody, translation)
+}
+
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false) 
+	flag = 0;
+    
+  }
+}
+
+
+
